663de8dc81cb2b6832fbd363fce8570f5dbab8cb
Tamoghno Kandar <55907205+tamoghnokandar@users.noreply.github.com>
2025-10-06 23:56:29 +0530

    Delete LLM RL Training Project directory

--- LLM RL Training Project/eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python -m venv grpo_evaluate
-source grpo_evaluate/bin/activate
-pip install uv
-uv pip install "sglang[all]>=0.5.3rc0"
-uv pip install transformers datasets
-apt-get update
-apt-get install libnuma-dev
-python3 eval_grpo.py
